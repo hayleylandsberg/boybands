720be1f4ced9feaa9ee05eb73180e148d1577a5b
Hayley Landsberg <hayleyheath@Hayleys-MacBook-Air.local>
2017-06-02 15:31:59 -0500

    Complete

--- boybands.js
+++ boybands.js
@@ -2,8 +2,15 @@ var bands = ["Boyz II Men", "NSync", "New Kids on the Block", "98 Degrees", "One
 var vegetables = ["Carrots", "Kale", "Zucchini", "Broccoli", "Squash"];
 
 // The number of loops to perform (what if the array changes?)
-var loopCount = 5;
+var loopCount = function() { 
+	if (bands.length >= vegetables.length) {
+		return bands.length;
+	} else {
+		return vegatables.length;
+	}
+};
 
+console.log(loopCount());
 // Keep track of which band we're on in the loop
 var currentBand = "";
 
@@ -11,19 +18,22 @@ var currentBand = "";
 var currentVeggie = "";
 
 // Get a reference to the appropriate DOM element for bands
-var bandElement = document.getElementById(???);
+var bandElement = document.getElementById("boy-bands");
 
 // Get a reference to the appropriate DOM element for vegetables
-var veggieElement = ???;
+var veggieElement = document.getElementById("vegetables");
 
 // Start looping
-for (var loopTracker = 0; loopTracker < loopCount; loopTracker += 1) {
+for (var i = 0; i < loopCount; i += 1) {
   
   // Add the band names into the correct <div>
-  currentBand = ???;
+  currentBand = [i];
 
   
   // Add the veggie names into the correct <div>
-  currentVeggie = ???;
+  currentVeggie = [i];
 
 }
+
+
+
